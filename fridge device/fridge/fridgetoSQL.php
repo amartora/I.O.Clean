321f18aab39462fff2adb81baced0cc2f9e215d3
amartora <46986576+amartora@users.noreply.github.com>
2021-03-28 07:33:22 -0400

    initial upload

---
+++ fridge device/fridge/fridgetoSQL.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = " ";
+// REPLACE with Database user
+$username = " ";
+// REPLACE with Database user password
+$password = " ";
+
+// Keep this API Key value to be compatible with the ESP32 code provided in the project page. 
+// If you change this value, the ESP32 sketch needs to match
+$api_key_value = " ";
+
+//set all variables beforehand to  empty
+$api_key= $temp= $humid= $counter= "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $api_key = test_input($_POST["api_key"]);
+    if($api_key == $api_key_value) {
+        $temp = test_input($_POST["temp"]);
+        $humid = test_input($_POST["humid"]);
+        $counter = test_input($_POST["counter"]);
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        
+        $sql = "INSERT INTO fridge (temp, humid, counter)
+        VALUES ('" . $temp . "', '" . $humid . "', '" . $counter . "')";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } 
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    
+        $conn->close();
+    }
+    else {
+        echo "Wrong API Key provided.";
+    }
+
+}
+else {
+    echo "Nothing to see here";
+    
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
